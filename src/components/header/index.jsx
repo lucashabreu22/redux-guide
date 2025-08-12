e5120228b2c8d905ae521cadd70c205640384d84
Lucas Abreu <lucashabreu22@gmail.com>
2025-08-12 21:34:39 +0100

    first commit

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,24 +1,48 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 // Components
 import Cart from "../cart/index";
 
 // Styles
 import * as Styles from "./styles";
+import UserActionTypes from "../../redux/user/action-types";
+
+import { loginUser, logoutUser } from "../../redux/user/actions";
+import { selectProductsCount } from "../../redux/cart/cart.selectors";
 
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
+  const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer);
+  const { products } = useSelector((rootReducer) => rootReducer.cartReducer);
+
+  const productCount = useSelector(selectProductsCount);
+
+  const dispatch = useDispatch();
+
   const handleCartClick = () => {
     setCartIsVisible(true);
+  };
+
+  const handleLoginCLick = () => {
+    dispatch(loginUser({ name: "Lucas", email: "lucas@lucas.com" }));
+  };
+
+  const handleLogoutClick = () => {
+    dispatch(logoutUser());
   };
 
   return (
     <Styles.Container>
       <Styles.Logo>Redux Shopping</Styles.Logo>
       <Styles.Buttons>
-        <div>Login</div>
-        <div onClick={handleCartClick}>Carrinho</div>
+        {currentUser ? (
+          <div onClick={handleLogoutClick}> Sair</div>
+        ) : (
+          <div onClick={handleLoginCLick}>Login</div>
+        )}
+        <div onClick={handleCartClick}>Carrinho ({productCount})</div>
       </Styles.Buttons>
 
       <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
---
+++ src/redux/cart/action-type.js
@@ -0,0 +1,7 @@
+const CartActionTypes = {
+  ADD_PRODUCT: "cart/addProduct",
+  REMOVE_PRODUCT: "cart/removeProduct",
+  INCREASE_PRODUCT_QUANTITY: "cart/increaseProductQuantity",
+  DECREASE_PRODUCT_QUANTITY: "cart/decreaseProductQuantity",
+};
+export default CartActionTypes;
